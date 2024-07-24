aece1492f379235220ce2e84d9ce1d4111e471f6
CarlosX559 <carlos.lipe559@hotmail.com>
2024-07-24 09:52:39 -0300

    send files

---
+++ js/script.js
@@ -0,0 +1,51 @@
+function menu() {
+
+    let open = document.querySelector(".menu_open");
+    let menu = document.getElementById("container_menu");
+    open.addEventListener("click", () => {
+        menu.style.display = "flex";
+        menu.style.left = "0px";
+        menu.style.animation = "move ease-in 400ms";
+    });
+
+
+
+    let close = document.querySelector(".close");
+
+    close.addEventListener("click", () => {
+        menu.style.display = "flex";
+        menu.style.left = "-100%";
+        menu.style.animation = "move ease-out 400ms";
+    });
+
+
+    let area_menu = document.querySelectorAll(".area_menu nav ul li a");
+
+    area_menu.forEach((element) => {
+        element.addEventListener("click", () => {
+            menu.style.left = "-100%";
+            menu.style.animation = "move ease-out 400ms";
+        });
+    });
+}
+menu();
+const animations = document.querySelectorAll("[data-animation]");
+const animationClass = "animate";
+
+function animation_scroll() {
+    const area_window = window.innerHeight * 0.21 * 3.8;
+
+    animations.forEach((element) => {
+        let posicaoAtual = element.getBoundingClientRect().top;
+
+        if (area_window > posicaoAtual) {
+            element.classList.add(animationClass);
+        } else {
+            element.classList.remove(animationClass);
+        }
+    });
+}
+
+if (animations.length) {
+    window.addEventListener("scroll", animation_scroll);
+}
